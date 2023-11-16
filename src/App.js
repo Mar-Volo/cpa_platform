bc7ee2ee593c1551fd96120046243cb6fef88d5a
Volodymyr Marchenko <Katalizator1992@gmail.com>
2023-11-16 03:16:47 +0200

    comment_some_code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+// import { lazy } from "react";
+// import { Route, Routes } from "react-router-dom";
 import ParticlesBackground from "./components/Background/ParticlesBackground";
 import { Header } from "./components/Header/Header";
 import GlobalStyle from "./GlobalStyle";
